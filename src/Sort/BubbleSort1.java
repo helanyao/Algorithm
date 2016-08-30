0d68f86972e6aef67bce13240cbbe415e2d4740c
Jinghua ZHU <zjhzhush@cn.ibm.com>
2016-08-30 10:34:00 +0800

    init

---
+++ src/Sort/BubbleSort1.java
@@ -0,0 +1,45 @@
+package Sort;
+
+public class BubbleSort1 {
+
+	public static void main(String[] args) {
+		char[] ca = {'5', '1', '3', '1', '7', '2', '8', '0', '9', '6'};
+		
+		BubbleSort(ca);
+		
+		PrintArray(ca);
+
+	}
+	
+	static void BubbleSort(char[] ca) {
+		boolean flag = true;
+		while(flag) {
+			flag = false;
+			for(int i = 0; i < ca.length - 1; i++) {
+				if(ca[i] > ca[i + 1]) {
+//					char tmp = ca[i];
+//					ca[i] = ca[i + 1];
+//					ca[i + 1] = tmp;
+					Swap(ca[i], ca[i + 1]);
+					flag = true;
+				}
+			}
+		}
+		PrintArray(ca);
+	}
+	
+	static void Swap(char a, char b) {
+		char tmp = a;
+		a = b;
+		b = tmp;
+	}
+	
+	static void PrintArray(char[] ca) {
+		System.out.println("");
+		for(int i = 0; i < ca.length; i++){
+			System.out.print(String.valueOf(ca[i]) + " ");
+		}
+		System.out.println("");
+	}
+
+}
