40c6c97ecff0c61150694e56ef60309f6c5aede9
Jinghua ZHU <jhzhu@outlook.com>
2016-10-13 21:06:59 +0800

    Given a linked list, remove the nth node from the end of list and return its head.

---
+++ src/LinkedList/RemoveNthFromEnd.java
@@ -0,0 +1,57 @@
+package LinkedList;
+
+import SingleLinkedList.LinkedList;
+import SingleLinkedList.Node;
+
+/*
+ * Given a linked list, remove the nth node from the end of list and return its head.
+ * For example, given linked list: 1->2->3->4->5, and n = 2.
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * 
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ */
+
+public class RemoveNthFromEnd {
+	public static void main(String[] args) {
+		Node n1 = new Node(0);
+		LinkedList ll = new LinkedList(n1);
+		//ll.add(1);
+		//ll.add(3);
+		ll.printAll();
+		ll.head = removeEnd(ll.getHead(), 1);
+		ll.printAll();
+	}
+	
+	public static Node removeEnd(Node head, int n) {
+		if(n <= 0 || head == null) {
+			return head;
+		}
+		
+		Node pre = null, p = head, q = head;
+		
+		for(int i = 0; i < n - 1; i++) {
+			if(q == null) {
+				throw new IllegalArgumentException("n exceeds the length of linked list.");
+			}
+			q = q.next;
+		}
+		
+		while(q.next != null) {
+			pre = p;
+			p = p.next;
+			q = q.next;
+		}
+		
+		if(pre == null) { // if remove the head node
+			pre = head;
+			head = head.next;
+			pre.next = null;
+		} else {
+			pre.next = p.next;
+			p.next = null;
+		}
+		
+		return head;
+	}
+
+}
