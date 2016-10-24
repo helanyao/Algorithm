a1dad42462a9ca846746395f9a9949b3945c37e3
Jinghua ZHU <jhzhu@outlook.com>
2016-10-24 10:15:34 +0800

    fix some bug of consistent hashing

--- src/Hash/ConsistentHash2.java
+++ src/Hash/ConsistentHash2.java
@@ -11,35 +11,36 @@ public class ConsistentHash2 {
 	private int microShards = 0;
 	private HashSet<Integer> nodeStatus = null;
 	private List<List<Integer>> machines = null;
+	private int minNodeId;
 
 	public static void main(String[] args) {
-		ConsistentHash2 ch = ConsistentHash2.create(100, 3);
+		ConsistentHash2 ch = ConsistentHash2.create(10, 5);
 		
 		List<Integer> item1 = ch.addMachine(1);
-		
-		System.out.println("*******");
-		for(int i = 0; i < item1.size(); i++) {
-			System.out.print(item1.get(i) + " ");
-		}
-		System.out.println("\n*******");
+//		
+//		System.out.println("*******");
+//		for(int i = 0; i < item1.size(); i++) {
+//			System.out.print(item1.get(i) + " ");
+//		}
+//		System.out.println("\n*******");
 		ch.printStatus();
 		System.out.println();
 		
-		System.out.println(ch.getMachineIdByHashCode(item1.get(1)));
+		System.out.println("get machie id for 4:" + ch.getMachineIdByHashCode(4));
 		
 		List<Integer> item2 = ch.addMachine(2);
-		
-		System.out.println("*******");
-		for(int i = 0; i < item2.size(); i++) {
-			System.out.print(item2.get(i) + " ");
-		}
-		System.out.println("\n*******");
+//		
+//		System.out.println("*******");
+//		for(int i = 0; i < item2.size(); i++) {
+//			System.out.print(item2.get(i) + " ");
+//		}
+//		System.out.println("\n*******");
 		ch.printStatus();
 		System.out.println();
 		
-		System.out.println(ch.getMachineIdByHashCode(item1.get(2)));
-		System.out.println(ch.getMachineIdByHashCode(item2.get(1)));
-		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("get machie id for " + i + ": " + ch.getMachineIdByHashCode(i));
+		}
 	}
 	
 	public void printStatus() {
@@ -61,7 +62,7 @@ public class ConsistentHash2 {
 	// In real NoSQL, n and k usually equal to 2^64 and 1000 respectively.
     public static ConsistentHash2 create(int n, int k) {
         if(n <= 0 || k <= 0 || k >= n) {
-        	throw new IllegalArgumentException();
+        	throw new IllegalArgumentException("Cycle length: " + n + " Micro-shards: " + k);
         }
         
         ConsistentHash2 ch = new ConsistentHash2();
@@ -69,6 +70,7 @@ public class ConsistentHash2 {
         ch.microShards = k;
         ch.nodeStatus = new HashSet<Integer>();
         ch.machines = new ArrayList<List<Integer>>();
+        ch.minNodeId = n;
         
         return ch;
     }
@@ -77,7 +79,7 @@ public class ConsistentHash2 {
     // @return a list of shard ids
     public List<Integer> addMachine(int machine_id) {
         if(machine_id < 0) { // assume machine_id starts from 0
-        	throw new IllegalArgumentException();
+        	throw new IllegalArgumentException("Machine ID: " + String.valueOf(machine_id));
         } else if(nodeStatus == null) {
         	System.out.println("node status is null.");
         	return null;
@@ -97,6 +99,9 @@ public class ConsistentHash2 {
         	while(nodeStatus.contains(index)) {
         		index = ra.nextInt(cycleLen);
         	}
+        	if(index < minNodeId) {
+        		minNodeId = index;
+        	}
         	nodeStatus.add(index);
         	machine.add(i, index);
         	// machine2.add(i, index);
@@ -111,26 +116,48 @@ public class ConsistentHash2 {
     // @param hashcode an integer
     // @return a machine id
     public int getMachineIdByHashCode(int hashcode) {
-        if(hashcode <= 0 || hashcode > cycleLen) {
-        	throw new IllegalArgumentException();
+        if(hashcode < 0 || hashcode > cycleLen) {
+        	throw new IllegalArgumentException("Hashcode: " +String.valueOf(hashcode));
         }
+        
+        int node = 0, distance = cycleLen;
         
         for(int i = 0; i < machines.size(); i++) {
         	List<Integer> machine = machines.get(i);
-        	int j = 0;
-        	for(; j < microShards; j++) {
-        		if(machine.get(j) == hashcode) {
-        			break;
+        	for(int j = 0; j < microShards; j++) {
+        		int nodeId = machine.get(j);
+        		if(nodeId == hashcode) {
+        			return getMachineByNode(nodeId);
+        		}
+        		if(nodeId > hashcode && (nodeId - hashcode) < distance) {
+        			node = nodeId;
+        			distance = nodeId - hashcode;
         		}
         	}
-        	if(j < microShards) {
-        		return machine.get(microShards);
-        	}
-        	
         }
-        
-        System.out.println("the node has not been set. Please check hashnode.");
-        return -1;
+        if(node == 0) {
+        	return getMachineByNode(minNodeId);
+        } else {
+        	return getMachineByNode(node);
+        }  
+    }
+    
+    public int getMachineByNode(int nodeId) {
+    	if(nodeId <0 || nodeId > cycleLen) {
+        	throw new IllegalArgumentException();
+        }
+    	
+    	for(int i = 0; i < machines.size(); i++) {
+    		List<Integer> machine = machines.get(i);
+    		for(int j = 0; j < microShards; j++) {
+    			if(machine.get(j) == nodeId) {
+    				return machine.get(microShards);
+    			}
+    		}
+    	}
+    	
+    	System.out.println("failed to find machine id for node: " + nodeId);
+    	return -1;
     }
 
 }
