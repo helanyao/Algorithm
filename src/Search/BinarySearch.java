23e1d0841f18e04cd822d46f71622bff0a4ee285
Jinghua ZHU <jhzhu@outlook.com>
2016-10-26 13:33:19 +0800

    update binary search to improve performance

--- src/Search/BinarySearch.java
+++ src/Search/BinarySearch.java
@@ -1,19 +1,19 @@
 package Search;
 
 public class BinarySearch {
-
 	public static void main(String[] args) {
-		int[] a = new int[]{1, 2, 3, 4};
-		System.out.println(search(a, 0, a.length - 1, 5));
+		int[] a = new int[]{1, 2};
+		int key = 2;
+		System.out.println(search(a, 0, a.length - 1, key));
+		System.out.println(searchN(a, key));
 	}
 	
 	public static int search(int[] arr, int low, int high, int val) {
 		if(arr == null || low < 0 || high < 0) {
-			throw new IllegalArgumentException("Argument value is illegal.");
+			return -1;
 		}
 		
 		if(high < low) {
-			System.out.println("Value " + val + " does not exist in input array.");
 			return -1;
 		}
 		
@@ -28,31 +28,37 @@ public class BinarySearch {
 		if(arr[mid] == val) {
 			return mid;
 		} else if(arr[mid] < val) {
-			return search(arr, mid + 1, high, val);
+			return search(arr, mid + 1, high - 1, val);
 		} else {
-			return search(arr, low, mid - 1, val);
+			return search(arr, low + 1, mid - 1, val);
 		}
 	}
 	
 	public static int searchN(int[] arr, int val) {
-		if(arr == null) {
-			throw new IllegalArgumentException("Array is null.");
+		if(arr == null || arr.length == 0) {
+			return -1;
 		}
 		
 		int low = 0, high = arr.length - 1, mid;
 		
 		while(low <= high) {
+			if(arr[low] == val) { // improve performance
+				return low;
+			} else if(arr[high] == val) {
+				return high;
+			}
 			mid = (low + high) / 2;
 			if(arr[mid] == val) {
 				return mid;
 			} else if(arr[mid] < val) {
 				low = mid + 1;
+				high--;
 			} else {
 				high = mid - 1;
+				low++;
 			}
 		}
 		
-		System.out.println("Value " + val + " does not exist in input array.");
 		return -1;
 	}
 
