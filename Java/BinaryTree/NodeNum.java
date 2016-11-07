87ded6e0f8ee0b038f74b89e69edb608dc99f90d
Jinghua ZHU <jhzhu@outlook.com>
2016-11-07 13:33:12 +0800

    get number of nodes in binary tree

---
+++ Java/BinaryTree/NodeNum.java
@@ -0,0 +1,48 @@
+package BinaryTree;
+
+import java.util.Stack;
+
+// get the number of nodes
+public class NodeNum {
+	public static void main(String[] args) {
+		String[] init = new String[]{"1", "[", "2", "[", "4", ",", "5", "]", ",", "3", "[", "6", ",", "]", "]"};
+		BTree bt = new BTree(init);
+		System.out.println(getNodeNum(bt.getRoot()));
+		System.out.println(getNodeNumN(bt.getRoot()));
+	}
+	
+	public static int getNodeNum(BNode root) {
+		if (root == null) {
+			return 0;
+		} else {
+			return 1 + getNodeNum(root.left) + getNodeNum(root.right);
+		}	
+	}
+	
+	public static int getNodeNumN(BNode root) {
+		int num = 0;
+		
+		if (root == null) {
+			return num;
+		}
+		
+		Stack<BNode> st = new Stack<BNode>();
+		BNode cur = root;
+		
+		while (cur != null || !st.isEmpty()) {
+			while (cur != null) {
+				num++;
+				if (cur.right != null) {
+					st.push(cur.right);
+				}
+				cur = cur.left;
+			}
+			
+			if (!st.isEmpty()) {
+				cur = st.pop();
+			}
+		}
+		
+		return num;
+	}
+}
