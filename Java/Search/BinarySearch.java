bd2bc58ae17fc6265bc54d3785ef1ef1ef93ee48
Jinghua ZHU <jhzhu@outlook.com>
2016-11-16 11:20:03 +0800

    modify binary search to make it be more friendly

--- Java/Search/BinarySearch.java
+++ Java/Search/BinarySearch.java
@@ -4,60 +4,65 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		int[] a = new int[]{1, 2};
 		int key = 2;
-		System.out.println(search(a, 0, a.length - 1, key));
+		System.out.println(findPosition(a, key));
 		System.out.println(searchN(a, key));
 	}
 	
-	public static int search(int[] arr, int low, int high, int val) {
-		if(arr == null || low < 0 || high < 0) {
-			return -1;
-		}
-		
-		if(high < low) {
-			return -1;
-		}
-		
-		// try to improve performance
-		if(arr[low] == val) {
-			return low;
-		} else if (arr[high] == val) {
-			return high;
-		}
-		
-		int mid = (low + high)/2;
-		if(arr[mid] == val) {
-			return mid;
-		} else if(arr[mid] < val) {
-			return search(arr, mid + 1, high - 1, val);
-		} else {
-			return search(arr, low + 1, mid - 1, val);
-		}
-	}
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public static int findPosition(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        return search(nums, target, 0, nums.length - 1);
+    }
+    
+    private static int search(int[] nums, int target, int low, int high) {
+        if (high < low) {
+            return -1;
+        } else if (nums[high] == target) {
+            return high;
+        } else if (nums[low] == target) {
+            return low;
+        }
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return search(nums, target, low + 1, mid - 1);
+        } else {
+            return search(nums, target, mid + 1, high - 1);
+        }
+        
+    }
 	
-	public static int searchN(int[] arr, int val) {
-		if(arr == null || arr.length == 0) {
+	public static int searchN(int[] nums, int target) {
+		if(nums == null || nums.length == 0) {
 			return -1;
 		}
 		
-		int low = 0, high = arr.length - 1, mid;
+		int low = 0, high = nums.length - 1, mid;
 		
-		while(low <= high) {
-			if(arr[low] == val) { // improve performance
-				return low;
-			} else if(arr[high] == val) {
-				return high;
-			}
-			mid = (low + high) / 2;
-			if(arr[mid] == val) {
-				return mid;
-			} else if(arr[mid] < val) {
-				low = mid + 1;
-				high--;
-			} else {
-				high = mid - 1;
-				low++;
-			}
-		}
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (nums[low] == target) {
+                return low;
+            } else if (nums[high] == target) {
+                return high;
+            } else if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+                high--;
+            } else {
+                low++;
+                high = mid - 1;
+            }
+        }
 		
 		return -1;
 	}
