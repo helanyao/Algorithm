a477c6f65cc5be9eb3be5217c243283ca8d12845
jinghuaz <jhzhu@outlook.com>
2017-05-24 11:06:59 +0800

    Update code style

--- Java/search/BinarySearch.java
+++ Java/search/BinarySearch.java
@@ -1,56 +1,29 @@
 package search;
 
 public class BinarySearch {
-	public static void main(String[] args) {
-		// int[] a = new int[]{1, 2, 2, 4, 6, 7};
-		int[] a = new int[]{1, 3, 3, 3, 5};
-		int key = 3;
-		System.out.println(findPosition(a, key));
-		System.out.println(searchN(a, key));
-		System.out.println(search9N(a, key));
-	}
-	
-    /**
-     * @param nums: An integer array sorted in ascending order
-     * @param target: An integer
-     * @return an integer
-     */
-    public static int findPosition(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+    public int search(int[] nums, int target, int low, int high) {
+        if (high < low)
             return -1;
-        }
-        
-        //return search(nums, target, 0, nums.length - 1);
-        return search9(nums, target, 0, nums.length - 1);
-    }
-    
-    private static int search(int[] nums, int target, int low, int high) {
-        if (high < low) {
-            return -1;
-        } else if (nums[high] == target) {
+        else if (nums[high] == target)
             return high;
-        } else if (nums[low] == target) {
+        else if (nums[low] == target)
             return low;
-        }
+            
         int mid = (high - low) / 2 + low;
-        if (nums[mid] == target) {
+        if (nums[mid] == target)
             return mid;
-        } else if (nums[mid] > target) {
+        else if (nums[mid] > target)
             return search(nums, target, low + 1, mid - 1);
-        } else {
+        else
             return search(nums, target, mid + 1, high - 1);
-        }  
     }
 	
-	public static int searchN(int[] nums, int target) {
-		if(nums == null || nums.length == 0) {
+	public int searchN(int[] nums, int target) {
+		if(nums == null || nums.length == 0)
 			return -1;
-		}
 		
 		int low = 0, high = nums.length - 1, mid;
-		
         while (low <= high) {
-        	System.out.println(low + " " + high);
             mid = (high - low) / 2 + low;
             if (nums[low] == target) {
                 return low;
@@ -66,39 +39,34 @@ public class BinarySearch {
                 high = mid - 1;
             }
         }
-        System.out.println(low + " " + high);
+        
 		return -1;
 	}
 	
-	public static int search9N(int[] nums, int target) {
-	    if (nums == null || nums.length == 0) {
+	public int search9N(int[] nums, int target) {
+	    if (nums == null || nums.length == 0)
 	        return -1;
-	    }
 	        
 	    int start = 0, end = nums.length - 1;
 	    while (start + 1 < end) {
-	    	System.out.println(start + " " + end);
-	        int mid = (end - start) / 2 + start; // (end + start)/2 may exceed the max of int
-	        if (nums[mid] == target) {
+	        int mid = (end - start) / 2 + start; // (end + start)/2 may exceed the max length of int
+	        if (nums[mid] == target)
 	            return mid;
-	        } else if (nums[mid] < target) {
+	        else if (nums[mid] < target)
 	            start = mid;
-	        } else {
+	        else
 	            end = mid;
-	        }
 	    }
-	    System.out.println(start + " " + end);
 	        
-	    if (nums[start] == target) {
+	    if (nums[start] == target)
 	        return start;
-	    } else if (nums[end] == target) {
+	    else if (nums[end] == target)
 	        return end;
-	    } else {
+	    else
 	        return -1;
-	    }
 	 }
 	
-	public static int search9(int[] nums, int target, int low, int high) {
+	public int search9(int[] nums, int target, int low, int high) {
 		if (low + 1 > high) 
 			return -1;
 		
@@ -110,5 +78,4 @@ public class BinarySearch {
 		else
 			return search9(nums, target, low, mid);
 	}
-
 }
